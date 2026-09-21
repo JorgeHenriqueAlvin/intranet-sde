2abbe10f82e453568a7ff35518cc2c7491df4da4
Jorge Henrique <73302052+JorgeHenriqueAlvin@users.noreply.github.com>
2026-09-21 16:26:43 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React,{useState,useEffect}from"react";
+Sistema Gestorimport React,{useState,useEffect}from"react";
 import{Search,ChevronLeft,ChevronRight,ExternalLink,FileText,Users,BriefcaseBusiness,Newspaper,CalendarDays,TrafficCone,MessageSquareText,BookOpen,Link2,Globe2,Network,LifeBuoy,Monitor}from"lucide-react";
 
 const defaultServices=[
